90834834f602ca29e7140e832109547786011649
Michael Shepard <mikeshepard04@gmail.com>
2019-01-17 20:26:54 -0600

    chore: add prerendering using Universal and ability to include server content/assets

--- server.ts
+++ server.ts
@@ -1,15 +1,14 @@
-// These are important and needed before anything else
 import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
 import { renderModuleFactory } from '@angular/platform-server';
 import { enableProdMode } from '@angular/core';
 
+import * as path from 'path';
+import * as fs from 'fs';
 import * as express from 'express';
 import { join } from 'path';
-import { readFileSync } from 'fs';
 
-// Faster server renders w/ Prod mode (dev mode never needed)
 enableProdMode();
 
 // Express server
@@ -19,9 +18,8 @@ const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
 // Our index.html we'll use as our template
-const template = readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
+const template = fs.readFileSync(join(DIST_FOLDER, 'browser', 'index.html')).toString();
 
-// * NOTE :: leave this as require() since this file is built Dynamically from webpack
 const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main');
 
 const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
@@ -31,7 +29,6 @@ app.engine('html', (_, options, callback) => {
     // Our index.html
     document: template,
     url: options.req.url,
-    // DI so that we can get lazy-loading to work differently (since we need it to just instantly render it)
     extraProviders: [
       provideModuleMap(LAZY_MODULE_MAP)
     ]
@@ -51,7 +48,52 @@ app.get('*', (req, res) => {
   res.render(join(DIST_FOLDER, 'browser', 'index.html'), { req });
 });
 
+const distFolder = join(DIST_FOLDER, 'browser');
+
+// TODO: Why is __dirname not working?
+const index = fs
+  .readFileSync(path.resolve('C:/dev/traveling-trotter', `${distFolder}/index.html`), 'utf8')
+  .toString();
+
+const paths = [
+  '/home',
+  '/run',
+  '/hike',
+  '/bio'
+];
+
 // Start up the Node server
 app.listen(PORT, () => {
   console.log(`Node server listening on http://localhost:${PORT}`);
+  try {
+    console.log('Rendering...');
+
+    // Run through each of the routes after the server has started so files from the server can be loaded via the http-interceptor
+    paths.forEach(p => renderToHtml(p, distFolder + p));
+    renderToHtml('/index.html', distFolder);
+  } catch (err) {
+    throw err;
+  }
 });
+
+// TODO: Put in separate file?
+function renderToHtml(url: string, folderPath: string): void {
+  renderModuleFactory(AppServerModuleNgFactory, {
+    url,
+    document: index,
+    extraProviders: [
+      provideModuleMap(LAZY_MODULE_MAP)
+    ],
+  }).then(html => {
+    if (url !== '/index.html') {
+      fs.mkdirSync(folderPath);
+    }
+    fs.writeFile(folderPath + '/index.html', html, (err => {
+      if (err) {
+        throw err;
+      } else {
+        console.log(folderPath + ` successfully renderd`);
+      }
+    }));
+  });
+}
--- src/app/app.server.module.ts
+++ src/app/app.server.module.ts
@@ -1,16 +1,20 @@
-import {NgModule} from '@angular/core';
-import {ServerModule} from '@angular/platform-server';
-import {ModuleMapLoaderModule} from '@nguniversal/module-map-ngfactory-loader';
+import { NgModule } from '@angular/core';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { ServerModule, ServerTransferStateModule } from '@angular/platform-server';
+import { ModuleMapLoaderModule } from '@nguniversal/module-map-ngfactory-loader';
+import { HttpInterceptorService } from './app.http-interceptor-service';
 
-import {AppModule} from './app.module';
-import {AppComponent} from './app.component';
+import { AppModule } from './app.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   imports: [
     AppModule,
     ServerModule,
-    ModuleMapLoaderModule
+    ModuleMapLoaderModule,
+    ServerTransferStateModule
   ],
-  bootstrap: [AppComponent],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: HttpInterceptorService, multi: true }],
+  bootstrap: [AppComponent]
 })
-export class AppServerModule {}
+export class AppServerModule { }
